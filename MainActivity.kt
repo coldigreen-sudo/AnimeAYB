f8b26586ff9ab33def4b18dda1c2861c71ad2999
coldigreen-sudo <coldigreen@gmail.com>
2025-09-21 17:11:55 +0200

    Add MainActivity class implementation

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tuusuario.animeayb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AnimeAYB)
+        setContentView(R.layout.activity_main)
+        // TODO: Configura RecyclerView, SearchView y navegación a detalles
+    }
+}
